7e40b016db2ba9fe050adf6759a885cdc202886e
Joshua <jykim92@gmail.com>
2021-03-10 22:28:33 -0500

    input

--- src/components/Blogform.js
+++ src/components/Blogform.js
@@ -3,6 +3,14 @@ import React from 'react'
 const Blogform = () => {
   return (
     <div>
+      <form className='blog-form'>
+        <input
+        className='input-form'
+        type="text"
+        placeholder="Add Blog"
+        />
+        <button className='blog-button'>Add</button>
+      </form>
       
     </div>
   )
