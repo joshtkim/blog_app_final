31189717c7aa982ac43eb4e13539881f56886738
Joshua <jykim92@gmail.com>
2021-03-17 18:11:35 -0400

    update with material ui

---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const sections = [
+  { title: 'Finance', url: '#' },
+  { title: 'Life', url: '#' },
+  { title: 'Culture', url: '#' },
+  { title: 'Business', url: '#' },
+  { title: 'Opinion', url: '#' },
+  { title: 'Health', url: '#' },
+  { title: 'Style', url: '#' },
+  { title: 'Travel', url: '#' },
+];
+
+const sidebar = {
+  title: 'About',
+  description: "Hi I'm Kelvin and I am a finance blogger",
+  social: [
+    { name: 'Twitter', icon: TwitterIcon },
+    { name: 'TikTok', icon: TikTokIcon },
+  ],
+};
+
+const Main = () => {
+
+  return (
+    <div>
+      <Container maxWidth="lg">
+        
+      </Container>
+      
+    </div>
+  )
+}
+
+export default Main
